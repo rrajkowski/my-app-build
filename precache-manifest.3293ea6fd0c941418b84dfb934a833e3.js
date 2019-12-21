ce0e9fbc7af5deb07b0cd36876007230f6fc72c0
Ruben Rajkowski <ruben.rajkowski@nbcuni.com>
2019-12-20 17:23:51 -0800

    update app path

---
+++ precache-manifest.3293ea6fd0c941418b84dfb934a833e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3f6f31846d673d8d145627596a25cf",
+    "url": "/my-app-build/index.html"
+  },
+  {
+    "revision": "714e2c2b0a6c41cb9203",
+    "url": "/my-app-build/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "82d51dbdd9d115f33b22",
+    "url": "/my-app-build/static/js/2.a371e7d7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app-build/static/js/2.a371e7d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "714e2c2b0a6c41cb9203",
+    "url": "/my-app-build/static/js/main.5aa84caf.chunk.js"
+  },
+  {
+    "revision": "b1321dd17c2e32b30eb7",
+    "url": "/my-app-build/static/js/runtime-main.54250a12.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app-build/static/media/logo.5d5d9eef.svg"
+  }
+]);
